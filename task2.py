dba9f738fa882c32275afa65dbbd8612a9cf2089
vinith2431 <vinith2431@gmail.com>
2025-06-19 12:13:20 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,8 @@
+numbers = list(range(1, 11))
+
+first_five = numbers[:5]
+reversed_list = first_five[::-1]
+
+print("orginal lisi:", numbers)
+print("First five elements:", first_five)
+print("Reversed list:", reversed_list)
